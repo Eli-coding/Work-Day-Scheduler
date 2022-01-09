969710a94ce3fa014f089245df72dc7c799666ea
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-01-08 20:38:16 -0400

    moment working

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ var timeDisplay = moment().format('MMMM Do YYYY, h:mm:ss a');
 var display = $('#currentDay');
 display.text(timeDisplay);
 
-   
+  
 
    
 
 /*
 
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
 
-WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 WHEN I click into a timeblock
 THEN I can enter an event
