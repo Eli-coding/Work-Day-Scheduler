294af31308b12a33569e4b9767ff9e3fafa226eb
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-01-08 20:13:54 -0400

    moment attempt

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-$(document).ready(function(){
 
-    $("currentDay").text (moment().format('MMMM Do YYYY, h:m:ss a'));
+//display time 
 
-    $(".saveBtn").click(function(){
+var timeDisplay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var display = $('#currentDay');
+display.text(timeDisplay);
 
+   
 
-    });
+   
 
-});
+/*
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+*/
