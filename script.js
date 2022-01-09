581d650d45f61b5707b48ad956bf003e5fd7da67
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-01-08 22:10:12 -0400

    color code time block

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 
 //display time 
-
 var timeDisplay = moment().format('MMMM Do YYYY, h:mm:ss a');
 var display = $('#currentDay');
 display.text(timeDisplay);
 
-  
+//check time for blocks
+function checkTime() {
+    var currentHour = moment().hour();
+    $(".time-block").each(function () {
 
-   
+        var time = parseInt($(this).attr("id"));
+
+        if (time < currentHour) {
+
+            $(this).addClass("past");
+
+        } else if (time === currentHour) {
+
+            $(this).removeClass("past");
+            $(this).addClass("present");
+
+        } else {
+
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+
+}
+
+
+
 
 /*
 
