f3fb5297ba6373b819810d980b259f2c70b142b5
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-01-15 22:50:28 -0400

    read me and local storage now works partially

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Work-Day-Scheduler
+# Work-Day-SchedulerGIVEN I am using a daily planner to create a schedule
+
+This web application is planner with current day is displayed at the top of the calender with timeblocks for standard business hours.
+The timeblocks for that day are color coded to indicate whether it is in the past, present, or future. I can enter an event in the timeblock and click save button. The text is saved in the local storage. Lastly, the saved events stayed even when the page is refresh. This was done using HTML, CSS and javascript.
+
+
--- script.js
+++ script.js
@@ -14,20 +14,20 @@ function checkTime() {
         if (time < currentHour) {
 
             $(this).addClass("past");
-            
+
 
         } else if (time === currentHour) {
 
             $(this).removeClass("past");
             $(this).addClass("present");
-            
+
 
         } else {
 
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
-            
+
         }
     });
 
@@ -35,28 +35,27 @@ function checkTime() {
 
 
 //save button for that time block and text saved in local strorage
-$( ".btn " ).click(function(event) {
+$(".btn ").click(function (event) {
     event.preventDefault();
-   
-   // console.log($(this))
-    //console.log($(this).siblings())
-    //console.log($(this).parent())
-    
-  
+
     var keyVar = $(this).parent("container"); //parent
     var inputVar = $(this).siblings("textarea").val(); //siblings
-    console.log(keyVar);
-    console.log(inputVar);
+   
+    localStorage.setItem("keyVar", JSON.stringify(inputVar));
 
-    localstorage.setItem(keyVar, inputVar);
-  
-  });
-    
-    
-  //1. Create a variable that targets the input value using $(this).siblings()  (as well as .val)
- // 2. create a variable that targets the div id that holds your input and buttion.  That will be similar but using parents
- // 3. save those to local storage localStorage.setItem( vorForID, varForInputValue)
+});
 
-// localstroage.setItem ("activitySaved", JSON.stringify (textarea)); 
-    //var activitySaved = localstroage.getItem ("activitySaved");
+
+////
+           ///// <div class="col-md-1 hour">10pm</div>
+           ///// <textarea  class="description col-md-10"></textarea>
+           ///// <button class="btn saveBtn col-md-1"><i class="far fa-save"></i> </button>
+
+       ///// </div>
+       //// <div id="23" class="row time-block">
+           ///// <div class="col-md-1 hour">11pm</div>
+            ////<textarea  class="description col-md-10"></textarea>
+          ////  <button class="btn saveBtn col-md-1"><i class="far fa-save"></i> </button>
+
+    //   /// </div>
 
