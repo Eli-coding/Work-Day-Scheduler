f474a91880f387090d4a107534d7c4f64b691e35
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-01-17 10:28:08 -0400

    save button works and it persist after refresh

--- assets/script.js
+++ assets/script.js
@@ -36,15 +36,31 @@ function checkTime() {
 
 //save button for that time block and text saved in local strorage
 $(".btn ").click(function (event) {
+    
+    
     event.preventDefault();
+    var keyVarId = $(this).parent().attr('id'); //getting the id of the parent
+    var inputVar = $(this).siblings('.description').val(); //siblings
 
-    var keyVar = $(this).parent("container"); //parent
-    var inputVar = $(this).siblings(".description").val(); //siblings
-   
-    localStorage.setItem(keyVar, JSON.stringify(inputVar));
+    console.log($(this).siblings('.description').val());
+    localStorage.setItem(keyVarId, JSON.stringify(inputVar));
+
 
 });
 
 
+//console.log(localStorage.getItem("9"));
+//console.log($("#9").children("textarea"));
+
+//items saved and persist after refreshing page.
+$("#9").children("textarea").val(localStorage.getItem("9"));
+$("#10").children("textarea").val(localStorage.getItem("10"));
+$("#11").children("textarea").val(localStorage.getItem("11"));
+$("#12").children("textarea").val(localStorage.getItem("12"));
+$("#13").children("textarea").val(localStorage.getItem("13"));
+$("#14").children("textarea").val(localStorage.getItem("14"));
+$("#15").children("textarea").val(localStorage.getItem("15"));
+$("#16").children("textarea").val(localStorage.getItem("16"));
+$("#17").children("textarea").val(localStorage.getItem("17"));
 
 
